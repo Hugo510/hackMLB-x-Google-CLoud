e37277f80059f889c7d133f92b512f59f05fc060
Jose Uriel Contreras Arevalos <128828694+Urielcont@users.noreply.github.com>
2025-01-26 00:09:58 -0600

    Convertir el arreglo resultante de la Bd en Objeto para coincidir con el Schema

--- src/helpers/spannerHelpers.ts
+++ src/helpers/spannerHelpers.ts
@@ -12,7 +12,12 @@ export async function runSingleRowQuery<T extends ZodTypeAny>(
     if (rows.length === 0) {
       return null;
     }
-    return schema.parse(rows[0]);
+    // Convertir el Arreglo resultante a objeto para manejo del schema
+    const rowObject = rows[0].reduce((acc: Record<string, any>, column: { name: string; value: any }) => {
+      acc[column.name] = column.value;
+      return acc;
+    }, {});
+    return schema.parse(rowObject);
   } catch (error) {
     logger.error(`Error al ejecutar consulta de una fila: ${error}`);
     throw error;
@@ -26,7 +31,17 @@ export async function runMultipleRowQuery<T extends ZodTypeAny>(
 ): Promise<z.infer<T>[]> {
   try {
     const [rows] = await database.run({ sql, params });
-    return rows.map((row) => schema.parse(row));
+
+    const transformedRows = rows.map((row) => {
+    const rowObject: Record<string, unknown> = {};
+    row.forEach((column: { name: string; value: unknown }) => {
+      rowObject[column.name] = column.value;
+    });
+    console.log("Usuario tranformados", rowObject)
+    return rowObject;
+  });
+
+  return transformedRows.map((rowObject) => schema.parse(rowObject));
   } catch (error) {
     logger.error(`Error al ejecutar consulta de múltiples filas: ${error}`);
     throw error;
