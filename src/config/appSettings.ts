0631886cb2fc22ca2429602aec21dc4da349db2a
Hugo Perez <hugoperez510@hotmail.com>
2025-02-04 10:54:30 -0600

    Convertir el puerto a un número entero al leer de las variables de entorno

--- src/config/appSettings.ts
+++ src/config/appSettings.ts
@@ -1,5 +1,5 @@
 export const appSettings = {
-  port: process.env.PORT || 8080,
+  port: parseInt(process.env.PORT || "8080", 10),
   baseUrl: process.env.MLB_STATS_BASEURL || "https://statsapi.mlb.com",
   // ...agregar más ajustes de la aplicación...
 };
